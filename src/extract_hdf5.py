703ad832240cc8daa7d7c0dcd8ac62324570eff0
navarrenicolas <navarrenicolas@gmail.com>
2022-10-29 22:24:00 +0200

    added 12 or 4 band decomposition

--- src/extract_hdf5.py
+++ src/extract_hdf5.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
-# nbands = 12
-# bands = list(range(nbands))
-bands = ["delta","theta","alpha","beta"]
+path12 = '../NTX-Hackathon/src/bands/bands12_calib.hdf5'
+path4 = '../NTX-Hackathon/src/bands/bands4_calib.hdf5'
 
-calibration_file = "./bands/band_calib.hdf5"
+bands12 = [f'P{i}' for i in range(12)]
+bands4 = ['delta','theta','alpha','beta']
 
-df = pd.concat([pd.read_hdf(calibration_file,key=key)["A1"] for key in bands],axis=1)
-df.columns = bands
+
+df = pd.concat([pd.read_hdf(bands4,key=key)["A1"] for key in bands4],axis=1)
+df.columns = bands4
+# df = pd.concat([pd.read_hdf(bands12,key=key)["A1"] for key in bands12],axis=1)
+# df.columns = bands12
